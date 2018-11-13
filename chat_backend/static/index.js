bd1256679ce7fa8bb99119ef4aaeb3132f776337
max <max.rok.98@gmail.com>
2018-11-13 11:26:13 +0200

    without history of chat

---
+++ chat_backend/static/index.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    // When connected, configure buttons
+    socket.on('connect', () => {
+
+        // Each button should emit a "submit vote" event
+        document.querySelector('#submit').onclick = () => {
+                //let user = document.querySelector('#name').innerHTML;
+                let chat_id = document.querySelector('#chat_id').innerHTML;
+                let text = document.querySelector('#text').value;
+                console.log(chat_id);
+                socket.emit('send message', {'chat_id': parseInt(chat_id, 10), 'text': text});
+                document.querySelector('#text').value = '';
+                return false;
+            };
+        });
+
+    // When a new vote is announced, add to the unordered list
+    socket.on('return massege', data => {
+        let chat_id = document.querySelector('#chat_id').innerHTML;
+        if (parseInt(data.chat_id) == parseInt(chat_id)) {
+            const li = document.createElement('li');
+            li.innerHTML = `${data.user}: ${data.text}`;
+            document.querySelector('#votes').append(li);
+        }
+    });
+});
